16c201775e75f0c2f4162f7aa6176630ff60714f
JangAWu <18325650330@163.com>
2023-04-18 17:47:40 +0800

    33

---
+++ forge.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  packagerConfig: {
+    icon: './images/icon@1x.ico'
+  },
+  rebuildConfig: {},
+  // 配置github
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'github-user-name',
+          name: 'github-repo-name',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        certificateFile: './cert.pfx',
+        iconUrl: './images/icon@1x.ico',
+        setupIcon: './images/icon@1x.ico',
+        certificatePassword: process.env.CERTIFICATE_PASSWORD,
+      },
+    },
+
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    // Linux
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          icon: './images/icon.png'
+        },
+      },
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+    {
+      // MAC
+      name: '@electron-forge/maker-dmg',
+      config: {
+        icon: './images/icon.icns'
+      },
+    },
+    {
+      name: '@electron-forge/maker-wix',
+      config: {
+        icon: './images/icon@1x.ico'
+      },
+    },
+  ],
+}
